aa3493e96199a0b43a4fe24fb8c99d7ebb72d0e9
risonakamo <hmykanae@gmail.com>
2020-02-24 22:57:05 -0500

    designing submit zone

--- popup/popupmain.tsx
+++ popup/popupmain.tsx
@@ -4,9 +4,14 @@ export default class PopupMain extends React.Component
   render()
   {
     return <>
-      <EntryRow name="Name" content="(C97) [Wasabi Mochi (Momosawa)] Le Malin-chan wa Sabori Jouzu? + Omake (Azur Lane)"/>
-      <EntryRow name="Group" content="wasabi mochi"/>
-      <EntryRow name="Type" content="NHSOMETHING" notEditable={true}/>
+      <div className="entry-rows">
+        <EntryRow name="Name" content="(C97) [Wasabi Mochi (Momosawa)] Le Malin-chan wa Sabori Jouzu? + Omake (Azur Lane)"/>
+        <EntryRow name="Group" content="wasabi mochi"/>
+        <EntryRow name="Type" content="NHSOMETHING" notEditable={true}/>
+      </div>
+      <div className="submit-zone">
+        <img className="submit-plus" src="../assets/addicon.svg"/>
+      </div>
     </>;
   }
 }
---
+++ thelist.md
@@ -0,0 +1,6 @@
+# next things
+- button to submit entry to database
+- program keyboard behaviour for popup text fields
+    - esc key resets to initial value
+    - enter moves to next field? or does nothing? or immediately submits it?
+    - tab definitely moves to next field, moving back to the top if last field
