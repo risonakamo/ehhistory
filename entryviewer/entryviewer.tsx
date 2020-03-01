9c4f1d5b0f5b1999c00fd3769f4ba8fa1fff2305
risonakamo <hmykanae@gmail.com>
2020-03-01 18:01:21 -0500

    first render of designed entries

--- entryviewer/entryviewer.tsx
+++ entryviewer/entryviewer.tsx
@@ -1,1 +1,10 @@
+window.onload=main;
+
+import EntryViewerMain from "./entryviewermain";
+
 import "./entryviewer.less";
+
+function main()
+{
+    ReactDOM.render(<EntryViewerMain/>,document.querySelector(".main"));
+}
---
+++ entryviewer/entryviewermain.tsx
@@ -0,0 +1,34 @@
+/* EntryViewerMain() */
+export default class EntryViewerMain extends React.Component
+{
+  render()
+  {
+    return <>
+      <div className="entrys">
+        <Entry/>
+        <Entry/>
+      </div>
+    </>;
+  }
+}
+
+/* Entry() */
+class Entry extends React.Component
+{
+  render()
+  {
+    return <div className="entry-row">
+      <div className="image-contain">
+        <div className="image-box no-image"></div>
+      </div>
+      <div className="content-contain">
+        <div className="content-inner">
+          <h1>A title that is not very long</h1>
+          <p className="groupname">groupname (1)</p>
+          <p className="tags"><span className="type1test">TYPENAME</span> TAG1 TAG2 TAG3</p>
+          <p className="date">2020/03/01</p>
+        </div>
+      </div>
+    </div>;
+  }
+}
