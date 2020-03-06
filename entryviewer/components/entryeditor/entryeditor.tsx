12605bceb1daab836d14eb1d9d9550f487b56b53
risonakamo <hmykanae@gmail.com>
2020-03-05 22:32:36 -0500

    add other fields field styling

--- entryviewer/components/entryeditor/entryeditor.tsx
+++ entryviewer/components/entryeditor/entryeditor.tsx
@@ -6,12 +6,22 @@ export default class EntryEditor extends React.Component
   {
     return <div className="entry-editor">
       <div className="editor-row">
+        <div className="field-name">NAME</div>
+        <input type="text"/>
+      </div>
+
+      <div className="editor-row">
         <div className="field-name">GROUP</div>
         <input type="text"/>
       </div>
 
       <div className="editor-row">
         <div className="field-name">URL</div>
+        <input type="text"/>
+      </div>
+
+      <div className="editor-row">
+        <div className="field-name">IMAGE</div>
         <input type="text"/>
       </div>
     </div>;
