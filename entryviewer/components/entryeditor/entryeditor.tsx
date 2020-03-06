d44ad02773d46c0c4c018752677b38460803d72e
risonakamo <hmykanae@gmail.com>
2020-03-06 00:48:02 -0500

    initial type design

--- entryviewer/components/entryeditor/entryeditor.tsx
+++ entryviewer/components/entryeditor/entryeditor.tsx
@@ -27,7 +27,24 @@ export default class EntryEditor extends React.Component
 
       <div className="editor-row">
         <div className="field-name">TYPE</div>
+        <div className="type-choice-hold">
+          <TypeChoice type="NHENTAI"/>
+          <TypeChoice type="OTHER"/>
+        </div>
       </div>
     </div>;
+  }
+}
+
+/* TypeChoice(EntryType type) */
+class TypeChoice extends React.Component
+{
+  props:{
+    type:EntryType
+  }
+
+  render()
+  {
+    return <div className={`type-choice ${this.props.type}`}>[{this.props.type}]</div>;
   }
 }
