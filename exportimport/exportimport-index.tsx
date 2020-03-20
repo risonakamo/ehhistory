3592dc6a1f5a8bdaf045a23ad71be1e9e3684fc6
risonakamo <hmykanae@gmail.com>
2020-03-20 01:04:08 -0400

    starting html for exportimport

--- exportimport/exportimport-index.tsx
+++ exportimport/exportimport-index.tsx
@@ -1,1 +1,19 @@
 import "./exportimport-index.less";
+
+class ExportImportMain extends React.Component
+{
+  render()
+  {
+    return <>
+      <p>import <input type="file"/></p>
+      <p><a href="">export</a></p>
+    </>;
+  }
+}
+
+function main()
+{
+  ReactDOM.render(<ExportImportMain/>,document.querySelector(".main"));
+}
+
+window.onload=main;
