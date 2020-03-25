3d9ce993722f515c30d812a9e393d213cf7b787b
risonakamo <hmykanae@gmail.com>
2020-03-25 00:20:19 -0400

    design tag input bar

--- entryviewer/components/tageditor/tageditor.tsx
+++ entryviewer/components/tageditor/tageditor.tsx
@@ -6,7 +6,12 @@ export default class TagEditor extends React.Component
   render()
   {
     return <div className="tag-editor">
-      <TagEditorTag/>
+      <div className="controls-zone">
+        <TagEditorInput/>
+      </div>
+      <div className="tags-hold">
+        <TagEditorTag/>
+      </div>
     </div>;
   }
 }
@@ -20,5 +25,14 @@ class TagEditorTag extends React.Component
       <p className="name">Something</p>
       <p>12</p>
     </div>;
+  }
+}
+
+/* TagEditorInput() */
+class TagEditorInput extends React.Component
+{
+  render()
+  {
+    return <input placeholder="new tag" type="text" className="tag-editor-input input-inherit"/>;
   }
 }
