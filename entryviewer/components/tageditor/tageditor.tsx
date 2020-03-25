0a1eea247e9cdc8ee03c912c9b88965d948deec8
risonakamo <hmykanae@gmail.com>
2020-03-25 00:35:57 -0400

    tag editor control buttons design

--- entryviewer/components/tageditor/tageditor.tsx
+++ entryviewer/components/tageditor/tageditor.tsx
@@ -7,6 +7,10 @@ export default class TagEditor extends React.Component
   {
     return <div className="tag-editor">
       <div className="controls-zone">
+        <div className="button-zone">
+          <TagEditorButton/>
+        </div>
+
         <TagEditorInput/>
       </div>
       <div className="tags-hold">
@@ -34,5 +38,17 @@ class TagEditorInput extends React.Component
   render()
   {
     return <input placeholder="new tag" type="text" className="tag-editor-input input-inherit"/>;
+  }
+}
+
+/* TagEditorButton() */
+class TagEditorButton extends React.Component
+{
+  render()
+  {
+    return <div className="tag-edit-button">
+      <img src="../imgs/checkmark-white.svg"/>
+      <p>Done</p>
+    </div>;
   }
 }
