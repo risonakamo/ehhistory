5592648045ada1cb0010452d33aa59d6405e02bf
risonakamo <hmykanae@gmail.com>
2020-03-25 00:47:55 -0400

    tag button variations

--- entryviewer/components/tageditor/tageditor.tsx
+++ entryviewer/components/tageditor/tageditor.tsx
@@ -1,5 +1,7 @@
 import "./tageditor.less";
 
+type TagEditorButtonType="done"|"cancel"|"filter"|"delete";
+
 /* TagEditor() */
 export default class TagEditor extends React.Component
 {
@@ -8,7 +10,10 @@ export default class TagEditor extends React.Component
     return <div className="tag-editor">
       <div className="controls-zone">
         <div className="button-zone">
-          <TagEditorButton/>
+          <TagEditorButton type="done" text="Done"/>
+          <TagEditorButton type="cancel" text="Cancel"/>
+          <TagEditorButton type="filter" text="AZ"/>
+          <TagEditorButton type="delete" text="Delete"/>
         </div>
 
         <TagEditorInput/>
@@ -41,14 +46,45 @@ class TagEditorInput extends React.Component
   }
 }
 
-/* TagEditorButton() */
+/* TagEditorButton(TagEditorButtonType type, string text) */
 class TagEditorButton extends React.Component
 {
+  props:{
+    type:TagEditorButtonType //the icon type
+    text:string //the text to display under it
+  }
+
   render()
   {
     return <div className="tag-edit-button">
-      <img src="../imgs/checkmark-white.svg"/>
-      <p>Done</p>
+      <img src={resolveTagButtonImage(this.props.type)}/>
+      <p>{this.props.text}</p>
     </div>;
   }
+}
+
+// return image path given a tag editor button type
+function resolveTagButtonImage(type:TagEditorButtonType):string
+{
+  var resolveImg:string;
+  switch (type)
+  {
+    case "done":
+      resolveImg="checkmark-white.svg";
+      break;
+
+    case "cancel":
+      resolveImg="close-white.svg";
+      break;
+
+    case "filter":
+      resolveImg="twoline-white.svg";
+      break;
+
+    case "delete":
+      resolveImg="close-circle-white.svg";
+      break;
+  }
+
+  return `../imgs/${resolveImg}`;
 }
