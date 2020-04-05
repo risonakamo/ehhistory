7d67bc1f689ab52a59a1f97147900435a6be3145
risonakamo <hmykanae@gmail.com>
2020-04-04 20:34:06 -0400

    button side bar design made buttons

--- entryviewer/components/buttonsidebar/buttonsidebar.tsx
+++ entryviewer/components/buttonsidebar/buttonsidebar.tsx
@@ -7,19 +7,25 @@ export default class ButtonSideBar extends React.Component
   {
     return <div className="button-side-bar">
       <div className="float-bar">
-        <ButtonSideBarButton/>
+        <ButtonSideBarButton imglink="../imgs/download-white.svg"/>
+        <ButtonSideBarButton imglink="../imgs/images-white.svg"/>
+        <ButtonSideBarButton imglink="../imgs/shuffle-white.svg"/>
       </div>
     </div>;
   }
 }
 
-/* ButtonSideBarButton() */
+/* ButtonSideBarButton(string imglink) */
 class ButtonSideBarButton extends React.PureComponent
 {
+  props:{
+    imglink:string
+  }
+
   render()
   {
     return <div className="button-side-bar-button">
-
+      <img src={this.props.imglink}/>
     </div>;
   }
 }
