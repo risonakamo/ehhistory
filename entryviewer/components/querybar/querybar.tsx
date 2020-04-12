1e7400fcb088527e07b421682b0b520a905289c7
risonakamo <hmykanae@gmail.com>
2020-04-12 16:49:00 -0400

    query string to query object function

--- entryviewer/components/querybar/querybar.tsx
+++ entryviewer/components/querybar/querybar.tsx
@@ -4,6 +4,12 @@ import TagEditorInput from "../tageditor/tageditor-bar";
 
 import "./querybar.less";
 
+interface SingleQuery
+{
+  type:keyof EntryQuery
+  query:string|EntryType
+}
+
 interface QueryBarProps
 {
 
@@ -22,6 +28,7 @@ export default class QueryBar extends React.PureComponent
   submitQuery(queryString:string):void
   {
     console.log("the query",queryString);
+    console.log("entry query",createEntryQuery(queryString));
   }
 
   render()
@@ -31,4 +38,64 @@ export default class QueryBar extends React.PureComponent
         newTag={this.submitQuery} noSubmit={true}/>
     </div>;
   }
+}
+
+// given query string, return entry query object
+function createEntryQuery(query:string):EntryQuery
+{
+  return _.reduce(query.split(" "),(r:EntryQuery,x:string):EntryQuery=>{
+    var parsedQuery:SingleQuery=parseSingleQuery(x);
+
+    if (parsedQuery.type=="tags" || parsedQuery.type=="subtractTags")
+    {
+      r[parsedQuery.type].push(parsedQuery.query);
+    }
+
+    else if (!r[parsedQuery.type])
+    {
+      r[parsedQuery.type]=parsedQuery.query as EntryType;
+    }
+
+    return r;
+  },{
+    tags:[],
+    subtractTags:[],
+    group:null,
+    type:null
+  });
+}
+
+// given a single query, return an object giving the type of that query and the query itself,
+// with any type denominators removed. an unknown query will just be a tag query
+function parseSingleQuery(query:string):SingleQuery
+{
+  var colonsplit=query.split(":");
+  if (colonsplit.length==1)
+  {
+    if (query[0]=="-")
+    {
+      return {
+        type:"subtractTags",
+        query:query.slice(1)
+      };
+    }
+
+    return {
+      type:"tags",
+      query
+    };
+  }
+
+  if (colonsplit[0]=="group" || colonsplit[0]=="type")
+  {
+    return {
+      type:colonsplit[0],
+      query:colonsplit[1]
+    };
+  }
+
+  return {
+    type:"tags",
+    query
+  };
 }
