da11c4d90741be0f95ea25b608a98dbf12cf24b2
risonakamo <hmykanae@gmail.com>
2020-07-25 23:52:15 -0400

    fix nh parser

--- pageparsers/nhparser.js
+++ pageparsers/nhparser.js
@@ -1,6 +1,6 @@
 (()=>{
     return {
         name:document.querySelector("#info h1").textContent,
-        group:document.querySelectorAll("#tags .tags")[3].firstElementChild.textContent.match(/(.*) \(/)[1]
+        group:document.querySelectorAll("#tags .tags")[3].firstElementChild.querySelector(".name").textContent
     };
 })();
